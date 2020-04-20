bad9df685305566bc5bcaac54025d6685bf24a69
Pranit5895 <guptapranit9@gmail.com>
2020-04-20 16:13:27 -0700

    2/9 functions declared

--- bankapp.cpp
+++ bankapp.cpp
@@ -17,7 +17,30 @@ public:
 	void report() const;	//functioon to show data
 	int retacno() const;	//function to return amount 
 	int retdeposit() const;	//function to return balance
-	char rettype() const;	//function to return type of account 
+	char rettype() const;	//function to return type of accountvoid account::create_account()
+{
+	cout<<"\nEnter The account No. :";
+	cin>>acno;
+	cout<<"\n\nEnter The Name of The account Holder : ";
+	cin.ignore();
+	cin.getline(name,50);
+	cout<<"\nEnter Type of The account (C/S) : ";
+	cin>>type;
+	type=toupper(type);
+	cout<<"\nEnter The Initial amount(>=500 for Saving and >=1000 for current ) : ";
+	cin>>deposit;
+	cout<<"\n\n\nAccount Created..";
+}
+
+void account::show_account() const
+{
+	cout<<"\nAccount No. : "<<acno;
+	cout<<"\nAccount Holder Name : ";
+	cout<<name;
+	cout<<"\nType of Account : "<<type;
+	cout<<"\nBalance amount : "<<deposit;
+}
+ 
 };
 
 
