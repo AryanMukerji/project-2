7a3e9458c75d5a48eb5158ab6efc0ff82f9a788d
Harshilbhardwaj47 <edwardkenway200017@gmail.com>
2020-04-20 11:38:56 -0700

    Checkings class debit card number and debit card pin completed

--- Bankapp/Checking.java
+++ Bankapp/Checking.java
@@ -10,11 +10,19 @@ public class checking extends Account {
 		Accountnumber = "2" + Accountnumber;
 //		System.out.println("NEW CHECKING ACCOUNT");
 //		System.out.println("ACCOUNT NUMBER: " + this.Accountnumber);
+		setdebitcard();
 	}
 
 	// list any methods specific to checking account
+	private void setdebitcard() {
+		debitcardnumber = (int) (Math.random() * Math.pow(10, 12));
+		debitcardpin = (int) (Math.random() * Math.pow(10, 4));
+	}
+
+
 	public void showifo() {
 		super.showifo();
 		System.out.println("ACCOUNT TYPE: CHECKING ACCOUNT");
+		System.out.println("Your debit card number: " + debitcardnumber + "\nyour debit card pin: " + debitcardpin);
 	}
 }
