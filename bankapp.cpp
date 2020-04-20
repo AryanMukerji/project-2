487aaf5242b933e33b792a02a4dfefb76350b273
Pranit5895 <guptapranit9@gmail.com>
2020-04-20 15:45:23 -0700

    Class Account declared without function definition

--- bankapp.cpp
+++ bankapp.cpp
@@ -1,3 +1,23 @@
 #include<iostream>
+#include<fstream>
+#include<iomanip>
 using namespace std;
+class account
+{
+	int acno;
+	char name[50];
+	int deposit;
+	char type;
+public:
+	void create_account();          
+	void show_account();
+	void modify();
+	void dep(int);
+	void draw(int);
+	void report() const;
+	int retacno() const;
+	int retdeposit() const;
+	char rettype() const;
+};
+
 
